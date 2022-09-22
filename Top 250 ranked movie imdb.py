bb5f5d669a298cd1728be4a320d4d82bd5f51e00
s0omit <54575702+s0omit@users.noreply.github.com>
2022-09-22 20:51:56 +0545

    Add files via upload

---
+++ Top 250 ranked movie imdb.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests, openpyxl
+
+excel = openpyxl.Workbook()
+print(excel)
+sheet = excel.active
+sheet.title = 'Top Rated Movie'
+print(excel.sheetnames)
+sheet.append(['Movie Rank','Movie Name','Year of Release','Rating'])
+
+try:
+    
+    source = requests.get('https://www.imdb.com/chart/top/')
+    source.raise_for_status()
+
+    soup = BeautifulSoup(source.text, 'html.parser')
+
+    movies = soup.find('tbody', class_="lister-list").find_all('tr')
+
+    for movie in movies:
+    
+        name = movie.find('td', class_="titleColumn").a.text
+    
+        rank = movie.find('td', class_="titleColumn").get_text(strip=True).split('.')[0]
+    
+        year = movie.find('span', class_="secondaryInfo").text.strip('()')
+    
+        rating = movie.find('td', class_="ratingColumn imdbRating").strong.text
+        print(rank, name, year, rating)
+        sheet.append([rank, name, year, rating])
+        
+
+except Exception as e:
+    print(e)
+
+excel.save('Top 250 Ranked Movie IMDB.xlsx')
